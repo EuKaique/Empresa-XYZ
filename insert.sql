5989692165b03d031290aa053034198781ba0f49
Kaique <34290569+EuKaique@users.noreply.github.com>
2020-10-28 11:56:14 -0300

    Add files via upload

---
+++ insert.sql
@@ -0,0 +1,40 @@
+INSERT INTO Departamentos VALUES(10,'RH','Sandra Ribeiro');
+INSERT INTO Departamentos VALUES(12,'Administrativo','Marcelo Teixeira');
+INSERT INTO Departamentos VALUES(15,'Financeiro','Bruna Junqueira');
+INSERT INTO Departamentos VALUES(18,'TI','André Almeida');
+INSERT INTO Departamentos VALUES(20,'Contabilidade','Thais Carvalho');
+INSERT INTO Departamentos VALUES(23,'Comercial','Daniel de Oliveira');
+
+INSERT INTO Funcionarios VALUES(1542,'Alex Barros','Auxiliar administrativo',1400,'2013-03-20');
+INSERT INTO Funcionarios VALUES(1424,'Amanda Silva','Recepcionista',1200,'2010-06-11');
+INSERT INTO Funcionarios VALUES(1325,'Brenner Oliveira','Analista de sistemas',3000,'2011-04-15');
+INSERT INTO Funcionarios VALUES(1645,'Bianca Costa','Assistente de DP',2650,'2010-09-05');
+INSERT INTO Funcionarios VALUES(1551,'Carlos Souza','Analista contábil',2870,'2010-06-11');
+INSERT INTO Funcionarios VALUES(1547,'Camila Alves','Auxliar admistrativo',1400,'2010-06-11');
+INSERT INTO Funcionarios VALUES(1601,'Danilo Gomes','Desenvolvedor',2500,'2014-05-10');
+INSERT INTO Funcionarios VALUES(1233,'Daniela Ribeiro','Produtora de Marketing',2910,'2014-05-10');
+INSERT INTO Funcionarios VALUES(1320,'Everton Rocha','Produtor de Marketing',2910,'2015-08-28');
+INSERT INTO Funcionarios VALUES(1417,'Emilly Melo','Gerente de Marketing',3775,'2010-06-11');
+INSERT INTO Funcionarios VALUES(1444,'Francisco Mendes','Analista de Suporte',1800,'2010-06-11');
+INSERT INTO Funcionarios VALUES(1212,'Fabiana Rodrigues','Promotora de Vendas',1200,'2013-12-01');
+INSERT INTO Funcionarios VALUES(1100,'Gabriel Almeida','Diretor financeiro',4300,'2012-07-07');
+
+INSERT INTO cargo VALUES(121,'Auxiliar Administrativo');
+INSERT INTO cargo VALUES(122,'Assistente Administrativo');
+INSERT INTO cargo VALUES(123,'Recepção');
+INSERT INTO cargo VALUES(124,'Telefonista');
+INSERT INTO cargo VALUES(101,'Analista de DP');
+INSERT INTO cargo VALUES(102,'Assistente de DP');
+INSERT INTO cargo VALUES(103,'Analista de RH');
+INSERT INTO cargo VALUES(104,'Assistente de RH');
+INSERT INTO cargo VALUES(153,'Diretor Financeiro');
+INSERT INTO cargo VALUES(154,'Economista');
+INSERT INTO cargo VALUES(155,'Analista Fiscal');
+INSERT INTO cargo VALUES(180,'Desenvolvedor');
+INSERT INTO cargo VALUES(182,'Analista de Sistemas');
+INSERT INTO cargo VALUES(184,'Analista de Suporte');
+INSERT INTO cargo VALUES(186,'Gerente de TI');
+INSERT INTO cargo VALUES(233,'Gerente de Marketing');
+INSERT INTO cargo VALUES(235,'Produtor(a) de Marketing');
+INSERT INTO cargo VALUES(237,'Promotor(a) de Vendas');
+
