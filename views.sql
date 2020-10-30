85275bf6efdf106836b01cf711d5de228129370d
Kaique <34290569+EuKaique@users.noreply.github.com>
2020-10-30 12:42:01 -0300

    Update views.sql

--- views.sql
+++ views.sql
@@ -5,3 +5,14 @@ d.nome AS Departamento,
 f.nome AS Funcionario
 FROM departamentos d, funcionarios f
 WHERE d.id_depto = f.id_depto;
+
+-- ESSA VIEW RETORNA TODOS OS FUNCIONÁRIOS COM SEU ID E CARGO
+CREATE VIEW cargoFunc AS
+SELECT c.id_cargo AS Id_Cargo,
+c.nome AS Cargo,
+f.id_func AS Id_Funcionario,
+f.nome AS Funcionario
+FROM cargo c, funcionarios f
+WHERE f.id_cargo = c.id_cargo;
+
+
