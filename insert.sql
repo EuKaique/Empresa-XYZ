edce3cd78eac8a166cd581d969a0febd2dd1447c
Kaique <34290569+EuKaique@users.noreply.github.com>
2020-11-03 19:10:06 -0300

    Update insert.sql

--- insert.sql
+++ insert.sql
@@ -1,4 +1,4 @@
-// INSERÇÃO DE DADOS NA TABELA Departamentos
+-- INSERÇÃO DE DADOS NA TABELA Departamentos
 INSERT INTO Departamentos VALUES(10,1018,'RH','Sandra Ribeiro');
 INSERT INTO Departamentos VALUES(12,1215,'Administrativo','Marcelo Teixeira');
 INSERT INTO Departamentos VALUES(15,1550,'Financeiro','Bruna Junqueira');
@@ -6,7 +6,7 @@ INSERT INTO Departamentos VALUES(18,1888,'TI','André Almeida');
 INSERT INTO Departamentos VALUES(20,2021,'Contabilidade','Thais Carvalho');
 INSERT INTO Departamentos VALUES(23,2324,'Comercial','Daniel de Oliveira');
 
-// INSERÇÃO DE DADOS NA TABELA Funcionarios
+-- INSERÇÃO DE DADOS NA TABELA Funcionarios
 INSERT INTO Funcionarios VALUES(1542,12,121,'Alex Barros','Auxiliar administrativo',1400,'2013-03-20');
 INSERT INTO Funcionarios VALUES(1424,12,123,'Amanda Silva','Recepcionista',1200,'2010-06-11');
 INSERT INTO Funcionarios VALUES(1325,18,182,'Brenner Oliveira','Analista de sistemas',3000,'2011-04-15');
@@ -21,7 +21,7 @@ INSERT INTO Funcionarios VALUES(1444,18,184,'Francisco Mendes','Analista de Supo
 INSERT INTO Funcionarios VALUES(1212,23,237,'Fabiana Rodrigues','Promotora de Vendas',1200,'2013-12-01');
 INSERT INTO Funcionarios VALUES(1100,15,153,'Gabriel Almeida','Diretor financeiro',4300,'2012-07-07');
 
-// INSERÇÃO DE DADOS NA TABELA Cargo
+-- INSERÇÃO DE DADOS NA TABELA Cargo
 INSERT INTO cargo VALUES(121,12,'Auxiliar Administrativo');
 INSERT INTO cargo VALUES(122,12,'Assistente Administrativo');
 INSERT INTO cargo VALUES(123,12,'Recepção');
