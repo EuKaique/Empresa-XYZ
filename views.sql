76d27d11a6c91035e708b8bb967748b94ff634a6
Kaique <34290569+EuKaique@users.noreply.github.com>
2020-11-05 14:56:04 -0300

    Update views.sql

--- views.sql
+++ views.sql
@@ -119,3 +119,17 @@ WHERE d.id_depto = f.id_depto
 AND c.id_depto = d.id_depto 
 AND f.id_cargo = c.id_cargo 
 AND d.nome = 'Contabilidade';
+
+-- ESSA VIEW RETORNA OS FUNCIONÁRIOS QUE TRABALHAM NO DEPARTAMENTO DE CONTABILIDADE
+CREATE VIEW deptoCOM AS
+SELECT d.id_depto,
+d.nome AS Departamento,
+lider,
+f.id_func,
+f.nome AS Funcionário,
+c.nome AS Cargo
+FROM Departamentos d, Funcionarios f, Cargo c
+WHERE d.id_depto = f.id_depto 
+AND c.id_depto = d.id_depto 
+AND f.id_cargo = c.id_cargo 
+AND d.nome = 'Comercial';
